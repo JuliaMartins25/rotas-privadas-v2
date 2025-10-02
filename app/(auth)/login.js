0c0228bebb6bf89cbff08ebebef1fb496d828a4e
Julia Martins <juliamartins.td2.senai@gmail.com>
2025-10-02 09:55:55 -0300

    feat: pagina auth login

---
+++ app/(auth)/login.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import { 
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    Alert,
+    KeyboardAvoidingView,
+    Platform,
+    ActivityIndicator,
+} from "react-native"
+import { useAuth } from "../../contexts/AuthContext";
+import { Link } from "expo-router";
+
+export default function LoginScreen() {
+    const [email, setEmail] = useState('');
+    const [password, sePassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const { singIn } = useAuth();
+
+    const handleLogin = async () => {
+        if (!email || !password) {
+            Alert.alert('Erro', 'Preencha todos o campos');
+            return;
+        }
+
+        setLoading(true);
+        try{
+            const result = await singIn(email, password);
+
+            if (!result.success) {
+                Alert.alert('Erro', result.message || 'Falha ao fazer login');
+            }
+        } catch (error) {
+            Alert.alert('Error', 'Falha ao fazer login');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+            <View style={styles.content}>
+                <Text style={styles.emoji}>🔐</Text>
+                <Text style={styles.title}>Bem-Vindo!</Text>
+                <Text style={styles.subtitle}>Faça login para continuar</Text>
+
+                <TextInput 
+                style={styles.input}
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                editable={!loading}
+                />
+
+                <TextInput
+                style={styles.input}
+                placeholder="Senha"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+                autoCapitalize="none"
+                editable={!loading}
+                />
+
+                <TouchableOpacity
+                style={[styles.button, loading && tyles.buttonDisabled]}
+                onPress={handleLogin}
+                disabled={loading}
+                >
+                    {loading ? (
+                        <ActivityIndicator color="#fff" />
+                    ) : (
+                        <Text style={styles.buttonText}>Entrar</Text>
+                    )}
+                </TouchableOpacity>
+
+                <View style={styles.registerConatiner}>
+                    <Text style={styles.regiterText}>Não tem conta?</Text>
+                    <Link href="/(auth)/register" asChild>
+                    <TouchableOpacity disabled={loading}>
+                        <Text style={styles.registerLink}>Cadastre-se</Text>
+                    </TouchableOpacity>
+                    </Link>
+                </View>
+
+                <Text style={styles.infoText}>
+                    Dica: Se não tiver conta, crie uma nova!
+                </Text>
+            </View>
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f5f5f5",
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+    }
+})
---
+++ app/index.js
@@ -0,0 +1,6 @@
+import { Rediract } from 'expo-router';
+
+export default function index() {
+
+    return <Rediract href="/(auth)/login"/>;
+} 
